7f5678cabaf4021e277feccbee1f6e25265d0695
Will Simpson <wsimpson@bignerdranch.com>
2022-06-10 18:14:42 -0700

    Initial add of files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simpsonw/nerdle
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var solution = []rune("slice")
+
+const emptySpace = " _ "
+const maxAttempts = 5
+
+func main() {
+	var gameOver = false
+	var numAttempts = 0
+	var result []string
+	for i := 0; i < len(solution); i++ {
+		result = append(result, emptySpace)
+	}
+	fmt.Printf("Welcome to Nerdle!  Try to guess the word in %d attempts\n", maxAttempts)
+	s := bufio.NewScanner(os.Stdin)
+	for !gameOver {
+		fmt.Printf("\n%s\n\n> ", strings.Join(result, ""))
+		s.Scan()
+		guess := strings.Trim(s.Text(), " ")
+		if len(guess) != 5 {
+			fmt.Println("Invalid guess")
+			continue
+		}
+
+		if numAttempts == maxAttempts {
+			fmt.Printf("Game over!  The correct answer was: %s\n", string(solution))
+			gameOver = true
+		} else {
+			runeGuess := []rune(guess)
+			for k, v := range runeGuess {
+				if v == solution[k] {
+					result[k] = fmt.Sprintf(" %c ", v)
+				} else {
+					result[k] = emptySpace
+				}
+			}
+		}
+
+		if guess == string(solution) {
+			fmt.Printf("\n%s\n\nCorrect!\n", strings.Join(result, ""))
+			gameOver = true
+		}
+
+	}
+}
